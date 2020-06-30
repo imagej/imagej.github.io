1e706ba3eeca44e335b55dae12d6fa013a65b85d
Edward Evans <echnoviru@gmail.com>
2020-06-30 10:32:51 -0500

    PEP standards for methods, added front matter

--- convert/ij_mw_preprocess.py
+++ convert/ij_mw_preprocess.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-def ReadFile(file_path):
+def read_file(file_path):
 
     #TODO: Sense box 'float = right' option and specifiy sidebox-right
 
@@ -16,17 +16,17 @@ def ReadFile(file_path):
 
     return content
 
-def FileName(file_path):
+def get_name(file_path):
 
     # returns file name of input file
-    file_name = os.path.basename(file_path)
+    name = os.path.basename(file_path)
 
-    if file_name.endswith('.mw'):
-        file_name = file_name[:-3]
+    if name.endswith('.mw'):
+        name = name[:-3]
 
-    return file_name
+    return name
 
-def GetTitle(file_path):
+def get_title(file_path):
 
     # returns title from file name and removes unwanted characters
     # update characters to remove as necessiary
@@ -40,7 +40,7 @@ def GetTitle(file_path):
 
     return title
 
-def GetBreadcrumb(file_path):
+def get_breadcrumb(file_path):
 
     # returns breadcrumb from the title
     breadcrumb = os.path.basename(file_path)
@@ -53,7 +53,7 @@ def GetBreadcrumb(file_path):
 
     return breadcrumb
 
-def GetCategories(file_path):
+def get_categories(file_path):
 
     # Reads the bottom of the file, finds categories and returns the formated front matter "categories".
     categories = []
@@ -74,7 +74,7 @@ def GetCategories(file_path):
 
     return str_categories
 
-def ProcessFile(file_path, str_content):
+def process_file(file_path, str_content):
 
     # perform regex replacements
     content_tmp = str_content
@@ -88,40 +88,39 @@ def ProcessFile(file_path, str_content):
     # replace '{{ stuff }}' mediawiki syntax with '{% include stuff %}` liquid
     content_tmp = re.sub(r'{{[ \n]*([A-Za-z0-9_]*)[ \n]*\|[ \n]*([^}]*)[ \n]*}}',r'{% include \1 content="\2" %}' ,content_tmp)
     content_tmp = re.sub(r'{{[ \n]*([A-Za-z0-9_]*)[ \n]*}}', r'%Replace% \1 %Replace% ', content_tmp)
-    content_tmp = ReplaceText('Notice', 'info-box', content_tmp)
-    content_tmp = ReplaceText('Warning', 'warning-box', content_tmp)
-    content_tmp = ReplaceText('Box', 'sidebox-right', content_tmp)
+    content_tmp = replace_text('Notice', 'info-box', content_tmp)
+    content_tmp = replace_text('Warning', 'warning-box', content_tmp)
+    content_tmp = replace_text('Box', 'sidebox-right', content_tmp)
     
-    print("processing " + str(FileName(file_path))+ ".mw...")
+    print("processing " + str(get_name(file_path))+ ".mw...")
 
     return content_tmp
 
-def ReplaceText(old_text, new_text, str_content):
+def replace_text(old_text, new_text, str_content):
 
     # replace old element calls to new element calls
     str_content = re.sub(r'{% include ' + old_text, r'{% include ' + new_text, str_content)
 
     return str_content
 
-def AddFrontMatter(str_content, file_path):
+def add_front_matter(str_content, file_path):
 
     # scrap necessary info fom page and populate the front matter.
-    title = GetTitle(file_path)
-    breadcrumb = GetBreadcrumb(file_path)
+    title = get_title(file_path)
+    breadcrumb = get_breadcrumb(file_path)
     author = "test author"
-    categories = GetCategories(file_path)
+    categories = get_categories(file_path)
     description = "test description"
     front_matter_content = "---\ntitle: {0}\nbreadcrumb: {1}\nlayout: page\nauthor: {2}\ncategories: {3}\ndescription: {4}\n---\n\n{5}".format(title, breadcrumb, author, categories, description, str_content)
 
     return front_matter_content
 
-def WriteFile(file_content, file_path):
+def write_file(file_content, file_path):
 
     # write file out
     save_path = "/home/edward/Documents/Development/Repos/LOCI/imagej.github.io/pages/"
     file_name = "test.mw"
     complete_name = os.path.join(save_path, file_name)
-    title = FileName(file_path)
 
     with open(complete_name, 'w') as f:
 
@@ -132,9 +131,21 @@ def WriteFile(file_content, file_path):
 
     return None
 
-input_file = "/home/edward/Documents/Workspaces/imagej-net-conversion/imagej_mediawiki_source/3D_Viewer.mw"
-#input_file = "/home/edward/Documents/Workspaces/imagej-net-conversion/imagej_mediawiki_source/Architecture.mw"
-file_contents = ReadFile(input_file)
-results = ProcessFile(input_file, file_contents)
-results = AddFrontMatter(results,  input_file)
-WriteFile(results, input_file)
+def run_pandoc(file_path):
+    
+    stream = os.popen('pwd')
+    output = stream.read()
+    print("This is the output: " + output)
+    
+    return None
+
+
+path = "/home/edward/Documents/Workspaces/imagej-net-conversion/imagej_mediawiki_source/3D_Viewer.mw"
+#path = "/home/edward/Documents/Workspaces/imagej-net-conversion/imagej_mediawiki_source/Architecture.mw"
+file_contents = read_file(path)
+output = process_file(path, file_contents)
+run_pandoc()
+write_file(output, path)
+
+# generate front matter and add to post-pandoc file
+fm = add_front_matter(file_contents, path)
