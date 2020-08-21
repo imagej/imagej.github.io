7c17c6a1b893406f18f937f0d3ee500fa45ee9a4
Sam Griffin <smgriffin2@wisc.edu>
2020-08-21 14:31:38 -0500

    Remove auto-conversion script logic
    We are done with the auto-conversion phase of the project.

--- convert/README.md
+++
@@ -1,5 +0,0 @@
-## Mass conversion
-```
-pip install markdown-link-extractor  
-python mass_convert.py
-```
--- convert/test_convert.py
+++
@@ -1,5 +0,0 @@
-from ij_mw_preprocess import *
-
-path_in = "/home/edward/Documents/Workspaces/imagej-net-conversion/imagej_mediawiki_source/3D_Viewer.mw"
-path_out = "/home/edward/Documents/Development/Repos/LOCI/imagej.github.io/pages/3D_Viewer.md"
-convert(path_in, path_out)
