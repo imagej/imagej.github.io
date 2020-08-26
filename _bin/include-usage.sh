f25935ebb9d5a2e3c06f257b581003385c5f6db0
Curtis Rueden <ctrueden@wisc.edu>
2020-08-26 12:44:16 -0500

    include-usage.sh: fix search query
    It had false negatives because includes can end without a space.

--- _bin/include-usage.sh
+++ _bin/include-usage.sh
@@ -2,5 +2,5 @@
 for include in _includes/*
 do
   short=${include#_includes/}
-  echo "$(git grep "{% *include *$short " | wc -l) :: $include"
+  echo "$(git grep "{% *include *$short[% ]" | wc -l) :: $include"
 done | sort -n
