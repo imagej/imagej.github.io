5d24c8c1e78197d9affa413cad14a5a5f7cfd8de
Curtis Rueden <ctrueden@wisc.edu>
2021-03-27 16:45:14 -0500

    Add a script to keep links up to date

---
+++ _bin/update-links.py
@@ -0,0 +1,32 @@
+import os
+
+def processfile(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    file_changed = False
+    while True:
+        line_changed = False
+        for old, new in redirects.items():
+            for i in range(len(lines)):
+                l = lines[i].replace(f'"{old}"', f'"/{new}"') # HTML, no slash
+                l = l.replace(f'({old})', f'(/{new})') # Markdown, no slash
+                l = l.replace(f'"/{old}"', f'"/{new}"') # HTML, with slash
+                l = l.replace(f'(/{old})', f'(/{new})') # Markdown, with slash
+                if lines[i] != l:
+                    file_changed = line_changed = True
+                    lines[i] = l
+        if not line_changed: break
+    if not file_changed: return
+    # write out file with updated links
+    with open(path, 'w') as f:
+        for line in lines:
+            f.write(line)
+
+with open('redirects.txt', 'r') as f:
+    lines = f.readlines()
+redirects = {l.split(' :: ')[0]: l.split(' :: ')[1].rstrip() for l in lines}
+
+for root, dirs, files in os.walk("_pages"):
+    for name in files:
+        processfile(os.path.join(root, name))
+
