5797209e8f37b57e6e393ddc6d5da1117e1d1350
Curtis Rueden <ctrueden@wisc.edu>
2021-04-14 11:54:09 -0500

    Improve the include-usage.sh script
    Now it:
    * Warns about includes that are used, but don't exist.
    * Prints a count of how many times each existing include is used.
    * Postfixes "[UNFINISHED]" for any template that is still a TODO.

--- _bin/include-usage.sh
+++ _bin/include-usage.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
-if [ $# -gt 0 ]
-then
-  for t in $@
-  do
-    grep -oh "{% *include  *$t [^%]*%}" _pages/*
-  done
-else
-  # List counts of all unfinished templates.
-  for include in _includes/*
-  do
-    grep -q TODO "$include" || continue
-    short=${include#_includes/}
-    echo "$(git grep "{% *include *$short[% ]" | wc -l) :: $include"
-  done | sort -n
-fi
+dir=$(cd "$(dirname "$0")/.." && pwd)
+cd "$dir"
+
+check_unfinished() {
+  grep -q TODO "$1" && echo "$2 [UNFINISHED]" || echo "$2"
+}
+
+matches=$(git grep -ohI '{% *include  *[a-z0-9-]*' | sed 's/.* //' | sort)
+used=$(echo "$matches" | uniq)
+
+# Check for usage of includes that don't exist.
+echo "$used" | while read line
+do
+  inc=${line##* }
+  test -f "_includes/$inc" || echo "[ERROR] Non-existent include used: $inc"
+done
+
+# Count how many times each include is used.
+count=$(echo "$matches" | uniq -c)
+for f in _includes/*
+do
+  inc=${f##*/}
+  s=$(echo "$count" | grep " $inc$")
+  test "$s" || s="      0 $inc"
+  grep -q TODO "$f" && echo "$s [UNFINISHED]" || echo "$s"
+done | sort -nr
