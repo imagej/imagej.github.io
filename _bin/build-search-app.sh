fb7ac8df23668ec421e813ec5914fb619f3db67f
Curtis Rueden <ctrueden@wisc.edu>
2021-04-25 16:23:21 -0500

    Add search.js.map, to shut the browser up
    The goal is to prevent the following warning in the console:
    
      Source map error: Error: request failed with status 404
      Resource URL: https://imagej.github.io/assets/js/search.js
      Source Map URL: /app.0972879d.js.map

--- _bin/build-search-app.sh
+++ _bin/build-search-app.sh
@@ -8,4 +8,5 @@ root=$(cd "$(dirname "$0")/.." && pwd)
 cd "$root/_search/client" &&
 rm -rf dist &&
 npm run-script build &&
-cp dist/app*.js "$root/assets/js/search.js"
+cat dist/app*.js | sed 's;sourceMappingURL=/.*\.map;sourceMappingURL=/assets/js/search.js.map;' > "$root/assets/js/search.js" &&
+cat dist/app*.js.map | sed 's;"app\.[0-9a-f]\+\.js";"search.js";' > "$root/assets/js/search.js.map"
