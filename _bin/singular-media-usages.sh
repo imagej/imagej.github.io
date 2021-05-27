c7d5117083499ce009b06f4ea51f815b449bca6e
Curtis Rueden <ctrueden@wisc.edu>
2021-05-26 20:53:30 -0500

    Improve singular-media-usages script
    Now it reports for *all* media files, not just those in the
    base /media folder, whether the file is in the correct place.

--- _bin/singular-media-usages.sh
+++ _bin/singular-media-usages.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-# Lists files in the base media/ folder that are only used by one folder of
-# _pages content. These should be moved to a corresponding folder under media/
+# Lists files in the media folder that are only used by one folder of _pages
+# content, but not residing in the correspondingly named folder under media/.
+# These should be moved to the corresponding folder under media/.
 
-for f in media/*
+find media -type f | while read f
 do
 sf=${f#media/}
 sf=${sf%.*}
+current=${f%/*}
+current=${current#media}
 dirs=$(grep -IR "$sf" _pages | sed 's;/[^/]*:.*;;' | sort -u)
-test "$dirs" -a "$(echo "$dirs" | wc -l)" -eq 1 && echo "$dirs :: $f"
+test "$dirs" -a "$(echo "$dirs" | wc -l)" -eq 1 -a "${dirs#_pages}" != "$current" &&
+  echo "$dirs :: $f"
 done
