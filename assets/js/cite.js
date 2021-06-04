c120bc42e9f4426f600ae6821ef0606619a24b74
Curtis Rueden <ctrueden@wisc.edu>
2021-06-03 20:22:44 -0500

    Convert DOIs to citations asynchronously
    This makes citation-heavy pages load waaay faster. :-)

--- assets/js/cite.js
+++ assets/js/cite.js
@@ -1,18 +1,18 @@
 const Cite = require('citation-js')
 
-function doiToHTML(element) {
+function fetchCitation(element) {
   let doi = element.getAttribute('data-citation-id');
   let style = element.getAttribute('data-citation-style');
-  if (doi == null || style == null) return null;
-  let citation = new Cite(doi);
-  return citation.format('bibliography', {
-    format: 'html',
-    template: style,
-    lang: 'en-US'
-  }).replace(/(https:\/\/doi\.org\/([^<]*))/, '<a href="$1">doi:$2</a>');
+  if (doi == null || style == null) return;
+  Cite.async(doi).then(function (citation) {
+    element.innerHTML = citation.format('bibliography', {
+      format: 'html',
+      template: style,
+      lang: 'en-US'
+    }).replace(/(https:\/\/doi\.org\/([^<]*))/, '<a href="$1">doi:$2</a>');
+  })
 }
 
 document.querySelectorAll(".citation").forEach(function(element) {
-  var html = doiToHTML(element);
-  if (html != null) element.innerHTML = html;
+  fetchCitation(element);
 });
