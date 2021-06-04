6a5edae71b5a97becdfc661b940e972dd2967910
Matt Wang <matt@matthewwang.me>
2021-06-04 16:07:36 -0700

    Adds test action; makes run-all-checks exit with proper status code

--- _bin/run-all-checks.sh
+++ _bin/run-all-checks.sh
@@ -3,6 +3,7 @@
 dir=$(cd "$(dirname "$0")/.." && pwd)
 bin="$dir/_bin"
 root="$dir/_site"
+any_failed=0
 test -d "$root" || {
   echo "Please generate the site first."
   echo "  bundle exec jekyll serve"
@@ -12,28 +13,36 @@ test -d "$root" || {
 echo "[Checking page generation]"
 "$bin/check-page-generation.sh"
 test $? -eq 0 && echo "--> Page generation looks good."
+test $? -eq 0 && any_failed=1
 
 echo
 echo "[Checking user IDs]"
 "$bin/check-user-ids.sh"
 test $? -eq 0 && echo "--> User IDs look good."
+test $? -eq 0 && any_failed=1
 
 echo
 echo "[Checking include usage]"
 "$bin/check-include-usage.sh"
 test $? -eq 0 && echo "--> Includes look good."
+test $? -eq 0 && any_failed=1
 
 echo
 echo "[Checking include documentation]"
 "$bin/check-include-help.sh"
 test $? -eq 0 && echo "--> Include docs look good."
+test $? -eq 0 && any_failed=1
 
 echo
 echo "[Checking HTML element id values]"
 "$bin/check-html-ids.sh"
 test $? -eq 0 && echo "--> HTML element ids look good."
+test $? -eq 0 && any_failed=1
 
 echo
 echo "[Checking site HTML]"
 "$bin/check-site-html.sh"
 test $? -eq 0 && echo "--> Site HTML looks good! Congratulations."
+test $? -eq 0 && any_failed=1
+
+exit $any_failed
