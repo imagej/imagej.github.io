117afb8361ad299ba55b64d2bee597165523b8e7
Matt Wang <matt@matthewwang.me>
2021-06-06 12:02:45 -0700

    fixes wrong cmp, comments out check-html

--- _bin/run-all-checks.sh
+++ _bin/run-all-checks.sh
@@ -13,36 +13,36 @@ test -d "$root" || {
 echo "[Checking page generation]"
 "$bin/check-page-generation.sh"
 test $? -eq 0 && echo "--> Page generation looks good."
-test $? -eq 0 && any_failed=1
+test $? -neq 0 && any_failed=1
 
 echo
 echo "[Checking user IDs]"
 "$bin/check-user-ids.sh"
 test $? -eq 0 && echo "--> User IDs look good."
-test $? -eq 0 && any_failed=1
+test $? -neq 0 && any_failed=1
 
 echo
 echo "[Checking include usage]"
 "$bin/check-include-usage.sh"
 test $? -eq 0 && echo "--> Includes look good."
-test $? -eq 0 && any_failed=1
+test $? -neq 0 && any_failed=1
 
 echo
 echo "[Checking include documentation]"
 "$bin/check-include-help.sh"
 test $? -eq 0 && echo "--> Include docs look good."
-test $? -eq 0 && any_failed=1
+test $? -neq 0 && any_failed=1
 
 echo
 echo "[Checking HTML element id values]"
 "$bin/check-html-ids.sh"
 test $? -eq 0 && echo "--> HTML element ids look good."
-test $? -eq 0 && any_failed=1
+test $? -neq 0 && any_failed=1
 
-echo
-echo "[Checking site HTML]"
-"$bin/check-site-html.sh"
-test $? -eq 0 && echo "--> Site HTML looks good! Congratulations."
-test $? -eq 0 && any_failed=1
+# echo
+# echo "[Checking site HTML]"
+# "$bin/check-site-html.sh"
+# test $? -eq 0 && echo "--> Site HTML looks good! Congratulations."
+# test $? -neq 0 && any_failed=1
 
 exit $any_failed
