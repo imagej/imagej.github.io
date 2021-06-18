37717104ed86287f904506f2062fdcf61d9b7f3d
Curtis Rueden <ctrueden@wisc.edu>
2021-06-18 12:16:34 -0500

    Move search.sh from server to client
    And update it to use the search-only API key.
    
    Now it can be used from any computer, not just the server.

--- _search/client/search.sh
+++ _search/client/search.sh
@@ -2,9 +2,11 @@
 
 # Search for a particular query string from the CLI. Useful for testing.
 
+dir=$(dirname "$(echo "$0")")
+
 hostname=search.imagej.net
 prefix="https://$hostname:8108"
 collection=imagej-wiki
 
-TYPESENSE_API_KEY=$(cat /etc/typesense/typesense-server.ini | grep '^api-key = ' | sed 's/^api-key = //')
+TYPESENSE_API_KEY=$(cat "$dir/src/app.js" | grep '^ *apiKey: ' | sed 's/.*apiKey: "\([^"]*\)".*/\1/')
 curl -H "X-TYPESENSE-API-KEY: ${TYPESENSE_API_KEY}" "$prefix/collections/$collection/documents/search?q=$1&query_by=title"
