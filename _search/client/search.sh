bf0eaa33639b7122b34b99d2db84b99a7b79750a
Curtis Rueden <ctrueden@wisc.edu>
2021-06-18 19:30:02 -0500

    search.sh: use the client's search-only key

--- _search/client/search.sh
+++ _search/client/search.sh
@@ -9,4 +9,7 @@ prefix="https://$hostname:8108"
 collection=imagej-wiki
 
 TYPESENSE_API_KEY=$(cat "$dir/src/app.js" | grep '^ *apiKey: ' | sed 's/.*apiKey: "\([^"]*\)".*/\1/')
-curl -H "X-TYPESENSE-API-KEY: ${TYPESENSE_API_KEY}" "$prefix/collections/$collection/documents/search?q=$1&query_by=title"
+
+which json_pp >/dev/null 2>&1 && pretty=json_pp || pretty=cat
+
+curl -fs -H "X-TYPESENSE-API-KEY: ${TYPESENSE_API_KEY}" "$prefix/collections/$collection/documents/search?q=$1&query_by=title" | $pretty
