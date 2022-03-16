16444360c17138359f36ab2f33ef30b258fa9dfc
Curtis Rueden <ctrueden@wisc.edu>
2022-03-16 12:51:33 -0500

    run-all-checks: return summed exit code
    So that we can use it with the CI.

--- _bin/run-all-checks.sh
+++ _bin/run-all-checks.sh
@@ -9,34 +9,45 @@ test -d "$root" || {
   exit 1
 }
 
+errors=0
+
 echo "[Checking page generation]"
-"$bin/check-page-generation.sh"
-test $? -eq 0 && echo "--> Page generation looks good."
+"$bin/check-page-generation.sh" &&
+   echo "--> Page generation looks good." ||
+  errors=$((errors+$?))
 
 echo
 echo "[Checking user IDs]"
-"$bin/check-user-ids.sh"
-test $? -eq 0 && echo "--> User IDs look good."
+"$bin/check-user-ids.sh" &&
+  echo "--> User IDs look good." ||
+  errors=$((errors+$?))
 
 echo
 echo "[Checking include usage]"
-"$bin/check-include-usage.sh"
-test $? -eq 0 && echo "--> Includes look good."
+"$bin/check-include-usage.sh" &&
+  echo "--> Includes look good." ||
+  errors=$((errors+$?))
 
 echo
 echo "[Checking include documentation]"
-"$bin/check-include-help.sh"
-test $? -eq 0 && echo "--> Include docs look good."
+"$bin/check-include-help.sh" &&
+  echo "--> Include docs look good." ||
+  errors=$((errors+$?))
 
 echo
 echo "[Checking HTML element id values]"
-"$bin/check-html-ids.sh"
-test $? -eq 0 && echo "--> HTML element ids look good."
+"$bin/check-html-ids.sh" &&
+  echo "--> HTML element ids look good." ||
+  errors=$((errors+$?))
 
 # NB: Disabled until check-page-html.sh caches its output
 # for the same input hash. Once that's in place, running
 # this test on CI won't be so super expensive.
 #echo
 #echo "[Checking site HTML]"
-#"$bin/check-site-html.sh"
-#test $? -eq 0 && echo "--> Site HTML looks good! Congratulations."
+#"$bin/check-site-html.sh" &&
+#  echo "--> Site HTML looks good! Congratulations." ||
+#  errors=$((errors+$?))
+
+test "$errors" -gt 255 && errors=255
+exit $errors
