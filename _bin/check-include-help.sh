a7d4ae5b0eb5e096027acfdfae7f929720355680
Curtis Rueden <ctrueden@wisc.edu>
2022-03-16 10:48:35 -0500

    check-include-help: return non-zero if errors

--- _bin/check-include-help.sh
+++ _bin/check-include-help.sh
@@ -6,14 +6,19 @@ cd "$dir"
 documented=$(grep -A9999 '# Available includes' _pages/editing/index.md |
   grep '^| \[' | sed 's/^| \[\([^]]*\)\].*/\1/')
 
+errors=0
+
 for f in _includes/*
 do
   test -d "$f" && continue
   inc=${f#_includes/}
-  echo "$documented" | grep -q "^$inc$" || echo "[ERROR] Undocumented: $inc"
+  echo "$documented" | grep -q "^$inc$" || { echo "[ERROR] Undocumented: $inc"; errors=$((errors+1)); }
 done
 
 echo "$documented" | while read inc
 do
-  test -f "_includes/$inc" || echo "[ERROR] Nonexistent: $inc"
+  test -f "_includes/$inc" || { echo "[ERROR] Nonexistent: $inc"; errors=$((errors+1)); }
 done
+
+test $errors -gt 255 && errors=255
+exit $errors
