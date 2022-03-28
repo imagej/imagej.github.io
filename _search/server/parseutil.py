0e4e13cd50ecf7999e84cec604a985ae1f41e76e
Jack Rueth <98347354+jackrueth@users.noreply.github.com>
2022-03-28 13:20:32 -0500

    search: trim long description lines
    Trims description lines over 500 characters.

--- _search/server/parseutil.py
+++ _search/server/parseutil.py
@@ -17,4 +17,10 @@ def first_sentence(lines):
     s = re.sub('\\s\\s*', ' ', s)                  # unify whitespace
     dot = s.find('. ')
     if dot >= 0: s = s[:dot+1]                     # cut down to first sentence
+    cutoff = 500
+    if len(s) > cutoff:
+        # trim long description line
+        last_space = s.rfind(" ", 0, cutoff - 2)
+        if last_space < 0: last_space = cutoff - 3
+        s = s[:last_space] + "..."
     return s.strip()
